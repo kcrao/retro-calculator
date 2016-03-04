e24d29580457259b702c1105f380a8d07a7f9266
kcrao <kcrao@hotmail.com>
2016-03-03 18:38:08 -0600

    Initial Commit

---
+++ retro-calcultor/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  retro-calcultor
+//
+//  Created by Kishore Rao on 3/3/16.
+//  Copyright © 2016 informed-planet.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
